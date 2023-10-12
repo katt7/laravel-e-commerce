4c8a791cd21eec443eb39b8c70dac6abe373e43e
Katteryn <kathryn7hv@gmail.com>
2023-10-11 22:19:59 -0400

    Mi primer commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+       //$products = DB::table ('products')->get();
+       $products = Product::all();
+       //dd($products);
+        return view ('products.index');
+    }
+    public function create()
+    {
+        return view ('products.create');
+    }
+    public function store()
+    {
+        //return 'ESTE ES EL FORMULARIO PARA CREAR PRODUCTOS';
+    }
+    public function show($product)
+    {
+        //$product = DB::table('products')->where('id', $product)->first();
+        //$product = DB::table('products')->find($product);
+        $product = Product::findOrfail($product);
+        dd($product);
+        return view("products.show");
+    }
+    public function edit($product)
+    {
+        return "MOSTRANDO EL PRODUCTO CON ID PARA EDITAR: $product";
+    }
+    public function update($product)
+    {
+       // return "MOSTRANDO EL PRODUCTO CON ID PARA EDITAR: $product";
+    }
+    public function destroy()
+    {
+        //return "MOSTRANDO EL PRODUCTO CON ID PARA EDITAR: $product";
+    }
+}
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AprendiendoLaravel</title>
+</head>
+<body>
+<h1>LISTA DE PRODUCTOS</h1>
+<div>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>TITLE</th>
+                <th>DESCRIPTION</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th>Gatos</th>
+                <th>Muy tiernos y apapachables</th>
+            </tr>
+            <tr>
+                <th>2</th>
+                <th>Perros</th>
+                <th>Alergres, imponenetes y protetores</th>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
