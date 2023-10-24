0721668c5be71b2662e220772b271decd39db72e
Katteryn <kathryn7hv@gmail.com>
2023-10-24 17:21:56 -0400

    se utilizo las rutas de recursos de laravel, se volvio a una sola linea las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,13 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('products', [ProductController::class, 'index'])->name('products.index');
-Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('products', [ProductController::class, 'store'])->name('products.store');
-Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
-Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::match(['put','pach'], 'products/{product}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+Route::resource('products', ProductController::class);
+
+// Route::get('products', [ProductController::class, 'index'])->name('products.index');
+// Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+// Route::post('products', [ProductController::class, 'store'])->name('products.store');
+// Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
+// Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+// Route::match(['put','pach'], 'products/{product}', [ProductController::class, 'update'])->name('products.update');
+// Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
 
 // si queremos una ruta para esto Route::resource('products', ProductController::class);
 
