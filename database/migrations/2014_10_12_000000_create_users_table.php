a24617747c8ed8f18072bf54cbdda8b6e3718faf
Katteryn <kathryn7hv@gmail.com>
2023-10-25 18:21:12 -0400

    se aumento el timestamp en la migracion de users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('admin_sice')->nullable();
             $table->rememberToken();
-            $table->timestamps();
-        });
+            });
     }
 
     /**
