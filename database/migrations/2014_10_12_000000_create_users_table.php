194fe06e72f14c6567b2d5907071c5b5af55c0a7
Katteryn <kathryn7hv@gmail.com>
2023-11-08 19:18:58 -0400

    se realizo el llenado de atributos en cada table, users, image, carts, orders y payments, y luego se migro a la base de datos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->timestamp('admin_sice')->nullable();
             $table->rememberToken();
+            $table->timestamps();
             });
     }
 
