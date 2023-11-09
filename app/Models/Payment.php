025b310c04fa3b7fac57fb83a68d7c0383afb79b
Katteryn <kathryn7hv@gmail.com>
2023-11-08 20:59:45 -0400

    se realizo la implementacion de los atributos en cada modelo segun las tablas creadas anteriormente

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'amount',
+        'payed_at',
+    ];
+    protected $dates = [
+        'payed_at',
+    ];
 }
